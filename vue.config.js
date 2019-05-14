fd3712c8abc3a5ea3619602da3f562ee07fbcc89
dingzu <349923359@qq.com>
2019-05-14 19:22:28 +0800

    添加端口

--- vue.config.js
+++ vue.config.js
@@ -39,7 +39,7 @@ module.exports = {
     open: process.platform === "darwin",
     disableHostCheck: false,
     host: "0.0.0.0",
-    port: 8089,
+    port: 80,
     https: false,
     hotOnly: false, // See https://github.com/vuejs/vue-cli/blob/dev/docs/cli-service.md#configuring-proxy
     proxy: {
